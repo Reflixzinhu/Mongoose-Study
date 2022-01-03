c0d1fa952f51b2dfb0a92a1f39914ba8bd0add30
João Victor <reflixplayshd@gmail.com>
2022-01-03 12:37:24 -0300

    Added

--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const linkController = require('../controllers/linkController');
 
+router.get('/all', linkController.allLinks);
+
 router.get('/:title', linkController.redirect);
+
+router.post(
+  '/',
+  express.urlencoded({ extended: true }),
+  linkController.addLink
+);
 
 module.exports = router;
